16fcb049aeff3c90d98903eb8d55e7a5433d99af
ramacobr <rafael.correa82@gmail.com>
2014-06-07 09:21:31 -0300

    Initial Commit

---
+++ Objects/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Objects
+//
+//  Created by Rafael M Corrêa on 07/06/14.
+//  Copyright (c) 2014 Rafael Corrêa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
